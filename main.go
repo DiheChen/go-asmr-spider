54bf0d2062d30996f84f6468cb57cb4985772c50
秋葉杏 <akegarasu.akiba@gmail.com>
2022-05-18 22:22:51 +0800

    Introduce multi threaded download support
    * update .gitignore
    
    * feat: use track struct instead of interface
    
    * feat: 支持单文件多线程下载
    feat: 支持多文件同时下载
    feat: 添加命令行参数控制
    feat: 添加 worker pool
    refactor: 更改 oauth 为 client
    refactor: 将整个 spider 抽象为一个 struct client
    refactor: 移动部分逻辑至utils

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -9,16 +10,30 @@ import (
 	"github.com/DiheChen/go-asmr-spider/spider"
 )
 
+var (
+	maxTask   int
+	maxThread int
+)
+
+func init() {
+	flag.IntVar(&maxTask, "w", 1, "最多同时进行的下载任务")
+	flag.IntVar(&maxThread, "t", 1, "单文件最大线程")
+}
+
 func main() {
+	flag.Parse()
+	fmt.Printf("正在使用 %d 线程, %d 任务同时下载\n", maxThread, maxTask)
 	fmt.Println("请输入要下载的音声的 RJ 号, 如: RJ373001, 如果要下载多个, 请用空格分开。")
 	readString, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	tasks := strings.Split(strings.TrimSpace(readString), " ")
-	s, err := spider.Login()
+	c := spider.NewASMRClient(maxTask, maxThread)
+	c.WorkerPool.Start()
+	err := c.Login()
 	if err != nil {
 		fmt.Println("登录失败:", err)
 		return
 	}
 	for _, task := range tasks {
-		s.Download(task)
+		c.Download(task)
 	}
 }
--- spider/spider.go
+++ spider/spider.go
@@ -3,9 +3,7 @@ package spider
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -18,45 +16,65 @@ import (
 
 var conf = config.GetConfig()
 
-type OAuth2 struct {
+type ASMRClient struct {
 	Authorization string
+	WorkerPool    *utils.WorkerPool
+	ThreadCount   int
 }
 
-func Login() (*OAuth2, error) {
+type track struct {
+	Type             string  `json:"type"`
+	Title            string  `json:"title"`
+	Children         []track `json:"children,omitempty"`
+	Hash             string  `json:"hash,omitempty"`
+	WorkTitle        string  `json:"workTitle,omitempty"`
+	MediaStreamURL   string  `json:"mediaStreamUrl,omitempty"`
+	MediaDownloadURL string  `json:"mediaDownloadUrl,omitempty"`
+}
+
+func NewASMRClient(maxTask int, maxThread int) *ASMRClient {
+	return &ASMRClient{
+		WorkerPool:  utils.NewWorkerPool(maxTask),
+		ThreadCount: maxThread,
+	}
+}
+
+func (ac *ASMRClient) Login() error {
 	payload, err := json.Marshal(map[string]string{
 		"name":     conf.Account,
 		"password": conf.Password,
 	})
 	if err != nil {
 		fmt.Println("登录失败, 配置文件有误。")
-		return nil, err
+		return err
 	}
 	client := utils.Client.Get().(*http.Client)
 	req, _ := http.NewRequest("POST", "https://api.asmr.one/api/auth/me", bytes.NewBuffer(payload))
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Referer", "https://www.asmr.one/")
-	req.Header.Set("User-Agent", "PostmanRuntime/7.29.0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36")
 	resp, err := client.Do(req)
 	utils.Client.Put(client)
 	if err != nil {
 		fmt.Println("登录失败, 网络错误。请尝试通过环境变量的方式设置代理。")
-		return nil, err
+		return err
 	}
 	defer func() { _ = resp.Body.Close() }()
 	all, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Print("登录失败, 读取响应失败。")
-		return nil, err
+		return err
 	}
 	res := make(map[string]string)
 	err = json.Unmarshal(all, &res)
-	return &OAuth2{Authorization: "Bearer " + res["token"]}, nil
+	ac.Authorization = "Bearer " + res["token"]
+	return nil
 }
 
-func (OAuth2 *OAuth2) GetVoiceTracks(id string) ([]interface{}, error) {
+func (ac *ASMRClient) GetVoiceTracks(id string) ([]track, error) {
 	client := utils.Client.Get().(*http.Client)
 	req, _ := http.NewRequest("GET", "https://api.asmr.one/api/tracks/"+id, nil)
-	req.Header.Set("Authorization", OAuth2.Authorization)
+	req.Header.Set("Authorization", ac.Authorization)
 	req.Header.Set("Referer", "https://www.asmr.one/")
 	req.Header.Set("User-Agent", "PostmanRuntime/7.29.0")
 	resp, err := client.Do(req)
@@ -71,79 +89,49 @@ func (OAuth2 *OAuth2) GetVoiceTracks(id string) ([]interface{}, error) {
 		fmt.Println("获取音声信息失败: ", err)
 		return nil, err
 	}
-	var res []interface{}
+	res := make([]track, 0)
 	err = json.Unmarshal(all, &res)
 	return res, nil
 }
 
-func DownloadFile(url string, dirPath string, fileName string) {
+func (ac *ASMRClient) Download(id string) {
+	id = strings.Replace(id, "RJ", "", 1)
+	fmt.Println("作品 RJ 号: " + id)
+	tracks, err := ac.GetVoiceTracks(id)
+	if err != nil {
+		fmt.Println("获取作品失败: " + err.Error())
+		return
+	}
+	ac.EnsureDir(tracks, "RJ"+id)
+	fmt.Println("下载完成。")
+}
+
+func (ac *ASMRClient) DownloadFile(url string, dirPath string, fileName string) {
 	if runtime.GOOS == "windows" {
 		for _, str := range []string{"?", "<", ">", ":", "/", "\\", "*", "|"} {
 			fileName = strings.Replace(fileName, str, "_", -1)
 		}
 	}
 	savePath := dirPath + "/" + fileName
-	if pathExists(savePath) {
+	if utils.PathExists(savePath) {
 		fmt.Println("文件已存在, 跳过。")
 		return
 	}
-	client := utils.Client.Get().(*http.Client)
 	fmt.Println("正在下载 " + savePath)
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Referer", "https://www.asmr.one/")
-	req.Header.Set("User-Agent", "PostmanRuntime/7.29.0")
-	resp, err := client.Do(req)
-	utils.Client.Put(client)
-	if err != nil {
-		fmt.Println("下载"+savePath+"失败: ", err)
-		return
-	}
-	defer func() { _ = resp.Body.Close() }()
-	file, err := os.OpenFile(dirPath+"/"+fileName+".temp", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Println("下载"+savePath+"失败: ", err)
-		return
-	}
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		fmt.Println("写入"+savePath+"失败: ", err)
-		return
-	}
-	_ = file.Close()
-	err = os.Rename(dirPath+"/"+fileName+".temp", dirPath+"/"+fileName)
-	if err != nil {
-		fmt.Println("重命名"+savePath+"失败: ", err)
-		return
-	}
-	return
+	downloader := utils.NewDownloader(url, dirPath, fileName, ac.ThreadCount, map[string]string{
+		"Referer": "https://www.asmr.one/",
+	})
+	ac.WorkerPool.TaskQueue <- downloader
 }
 
-func EnsureDir(tracks []interface{}, basePath string) {
+func (ac *ASMRClient) EnsureDir(tracks []track, basePath string) {
 	path := basePath
 	_ = os.MkdirAll(path, os.ModePerm)
-	for _, track := range tracks {
-		if track.(map[string]interface{})["type"].(string) != "folder" {
-			DownloadFile(track.(map[string]interface{})["mediaDownloadUrl"].(string), path, track.(map[string]interface{})["title"].(string))
+	for _, t := range tracks {
+		if t.Type != "folder" {
+			ac.DownloadFile(t.MediaDownloadURL, path, t.Title)
 		} else {
-			_ = os.MkdirAll(path+"/"+track.(map[string]interface{})["title"].(string), os.ModePerm)
-			EnsureDir(track.(map[string]interface{})["children"].([]interface{}), path+"/"+track.(map[string]interface{})["title"].(string))
+			ac.EnsureDir(t.Children, fmt.Sprintf("%s/%s", path, t.Title))
 		}
 	}
-}
-
-func pathExists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil || errors.Is(err, os.ErrExist)
-}
-
-func (OAuth2 *OAuth2) Download(id string) {
-	id = strings.Replace(id, "RJ", "", 1)
-	fmt.Println("作品 RJ 号: " + id)
-	tracks, err := OAuth2.GetVoiceTracks(id)
-	if err != nil {
-		fmt.Println("获取作品失败: " + err.Error())
-		return
-	}
-	EnsureDir(tracks, "RJ"+id)
-	fmt.Println("下载完成。")
 }
