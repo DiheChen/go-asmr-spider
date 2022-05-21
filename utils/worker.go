4a1ee9c3ded63c8a8beb77f4c7a67456618bf9e8
AkiraXie <l997460364@outlook.com>
2022-05-22 05:26:15 +0800

    remove some useless code

--- utils/worker.go
+++ utils/worker.go
@@ -7,18 +7,10 @@ import (
 
 type WorkerChan chan *MultiThreadDownloader
 
-type Worker struct {
-	TaskChan WorkerChan
-}
-
 type WorkerPool struct {
 	sync.WaitGroup
 	WorkerCount int
 	TaskQueue   WorkerChan
-}
-
-func NewWorker() *Worker {
-	return &Worker{TaskChan: make(chan *MultiThreadDownloader)}
 }
 
 func NewWorkerPool(WorkerCount int) *WorkerPool {
@@ -28,24 +20,19 @@ func NewWorkerPool(WorkerCount int) *WorkerPool {
 	}
 }
 
-func (w *Worker) Run(owner *WorkerPool) {
-	for t := range owner.TaskQueue {
-		owner.Add(1)
-		go func(t *MultiThreadDownloader) {
-			err := t.Download()
-			if err != nil {
-				fmt.Printf("下载 %s 时出现错误 %s\n", t.FullPath, err)
-				return
-			}
-			fmt.Println("下载完成", t.FullPath)
-			owner.Done()
-		}(t)
-	}
-}
-
 func (wp *WorkerPool) Start() {
-	for i := 0; i < wp.WorkerCount; i++ {
-		w := NewWorker()
-		go w.Run(wp)
-	}
+	go func() {
+		for t := range wp.TaskQueue {
+			wp.Add(1)
+			go func(t *MultiThreadDownloader) {
+				err := t.Download()
+				if err != nil {
+					fmt.Printf("下载 %s 时出现错误 %s\n", t.FullPath, err)
+					return
+				}
+				fmt.Println("下载完成", t.FullPath)
+				wp.Done()
+			}(t)
+		}
+	}()
 }
