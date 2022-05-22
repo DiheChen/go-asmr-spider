32606a403fb5a2ec2c92cec67378f3683cf1fab6
AkiraXie <l997460364@outlook.com>
2022-05-22 14:55:15 +0800

    remove resque

--- utils/worker.go
+++ utils/worker.go
@@ -16,7 +16,6 @@ func NewWorkerPool(WorkerCount int) *WorkerPool {
 	return &WorkerPool{
 		WorkerCount: WorkerCount,
 		TaskQueue:   make(WorkerChan, WorkerCount),
-		ResQueue:    make(chan bool, WorkerCount),
 	}
 }
 
@@ -27,19 +26,10 @@ func (wp *WorkerPool) Start() {
 				err := t.Download()
 				if err != nil {
 					fmt.Printf("下载 %s 时出现错误 %s\n", t.FullPath, err)
-					wp.ResQueue <- false
 					return
 				}
 				fmt.Println("下载完成", t.FullPath)
-				wp.ResQueue <- true
 			}
 		}()
 	}
-	go func() {
-		for {
-			select {
-			case <-wp.ResQueue:
-			}
-		}
-	}()
 }
